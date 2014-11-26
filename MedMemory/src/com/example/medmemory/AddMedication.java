cc8ab0d2531464f82b321d62ecec58aea44925af
Stephen <Sack11man@aol.com>
2014-11-25 21:42:52 -0600

    2nd Layout
    Updates

---
+++ MedMemory/src/com/example/medmemory/AddMedication.java
@@ -0,0 +1,16 @@
+package com.example.medmemory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddMedication extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addmedication);
+	}
+	
+	
+	
+}
