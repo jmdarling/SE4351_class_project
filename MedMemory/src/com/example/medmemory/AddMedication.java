d42a5cf5251b1789c803703912c789e91cfb48b6
Rahat Ahmed <rahatarmanahmed@gmail.com>
2014-11-26 02:12:02 -0600

    Added images and dosage field + editing medication

--- MedMemory/src/com/example/medmemory/AddMedication.java
+++ MedMemory/src/com/example/medmemory/AddMedication.java
@@ -1,13 +1,23 @@
 package com.example.medmemory;
 
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.sql.Date;
 import java.util.Calendar;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -16,14 +26,26 @@ import com.example.medmemory.model.Medication;
 
 public class AddMedication extends Activity {
 
+	private static final int SELECT_PICTURE = 1;
+	public static final String MEDICATION_ID = "MEDICATION_ID";
+	
 	TextView medName;
+	TextView dosage;
 	TextView pillCount;
 	TextView notes;
+	
 	TimePicker timePicker;
+	
+	ImageView imageView;
 
 	Button saveBtn;
+	Button selectImageBtn;
 	
 	Date reminderDate;
+	
+	Bitmap image;
+	
+	Medication editMed;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +53,11 @@ public class AddMedication extends Activity {
         setContentView(R.layout.addmedication);
         
         medName = (TextView) findViewById(R.id.med_name);
+        dosage = (TextView) findViewById(R.id.med_dosage);
         pillCount = (TextView) findViewById(R.id.med_count);
         notes = (TextView) findViewById(R.id.med_notes);
         timePicker = (TimePicker) findViewById(R.id.timePicker);
+        imageView = (ImageView) findViewById(R.id.med_image);
         
         saveBtn = (Button) findViewById(R.id.save_med_btn);
         saveBtn.setOnClickListener(new OnClickListener() {
@@ -41,41 +65,133 @@ public class AddMedication extends Activity {
 			@Override
 			public void onClick(View v)
 			{
+				// Default data for testing purposes
 				if(medName.getText().length() == 0)
 					medName.setText("Aspirin");
+				if(dosage.getText().length() == 0)
+					dosage.setText("100mg");
 				if(pillCount.getText().length() == 0)
 					pillCount.setText("30");
 				if(notes.getText().length() == 0)
 					notes.setText("Take with food, do not take with orange juice.");
 				
+				// Debug log
 				System.out.println("====SAVING MED====");
 				System.out.println("Name: "+medName.getText());
+				System.out.println("Dosage: "+dosage.getText());
 				System.out.println("Pill Count: "+pillCount.getText());
 				System.out.println("Notes: "+notes.getText());
 				System.out.println("Reminder Time: "+timePicker.getCurrentHour()+":"+timePicker.getCurrentMinute());
 				
-				Medication med = new Medication();
-				med.setName(medName.getText().toString());
-				med.setMaximumPillCount(Integer.parseInt(pillCount.getText().toString()));
-				med.setCurrentPillCount(Integer.parseInt(pillCount.getText().toString()));
-				med.setNotes(notes.getText().toString());
+				if(editMed == null)
+				{
+					// Create medication
+					Medication med = new Medication();
+					med.setName(medName.getText().toString());
+					med.setDosage(dosage.getText().toString());
+					med.setMaximumPillCount(Integer.parseInt(pillCount.getText().toString()));
+					med.setCurrentPillCount(Integer.parseInt(pillCount.getText().toString()));
+					med.setNotes(notes.getText().toString());
+					med.setImage(image);
+					
+					Calendar cal = Calendar.getInstance();
+					cal.set(Calendar.HOUR_OF_DAY,timePicker.getCurrentHour());
+					cal.set(Calendar.MINUTE,timePicker.getCurrentMinute());
+					cal.set(Calendar.SECOND,0);
+					cal.set(Calendar.MILLISECOND,0);
+					med.setReminderDate(cal.getTime());
+					
+					Database.context = AddMedication.this;
+					boolean success = Database.addMedication(med);
+					System.out.println("Added med successfully? "+success);
+				}
+				else
+				{
+					Database.context = AddMedication.this;
+					Database.deleteMedicationById(editMed.getId());
+					boolean success = Database.addMedication(editMed);
+					System.out.println("Added med successfully? "+success);
+				}
 				
-				Calendar cal = Calendar.getInstance();
-				cal.set(Calendar.HOUR_OF_DAY,timePicker.getCurrentHour());
-				cal.set(Calendar.MINUTE,timePicker.getCurrentMinute());
-				cal.set(Calendar.SECOND,0);
-				cal.set(Calendar.MILLISECOND,0);
-				med.setReminderDate(cal.getTime());
+				// Add to DB
 				
-				Database.context = AddMedication.this;
-				boolean success = Database.addMedication(med);
-				System.out.println("Added med successfully? "+success);
 				
+				setResult(RESULT_OK);
 				finish();
 			}
 		});
+        
+        selectImageBtn = (Button) findViewById(R.id.select_image_btn);
+        selectImageBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v)
+			{
+				Intent pickIntent = new Intent();
+				pickIntent.setType("image/*");
+				pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+				Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+				String pickTitle = "Select Image";
+				Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+				chooserIntent.putExtra
+				(
+				  Intent.EXTRA_INITIAL_INTENTS, 
+				  new Intent[] { takePhotoIntent }
+				);
+
+				startActivityForResult(chooserIntent, SELECT_PICTURE);
+			}
+		});
+        
+        
+        Intent intent = getIntent();
+        int medId = intent.getIntExtra(MEDICATION_ID, -1);
+        if(medId != -1)
+        {
+        	Database.context = this;
+        	editMed = Database.getMedicationById(medId);
+			medName.setText(editMed.getName());
+			dosage.setText(editMed.getDosage());
+			pillCount.setText(""+editMed.getCurrentPillCount()); 
+			notes.setText(editMed.getNotes());
+			image = editMed.getImage();
+			imageView.setImageBitmap(editMed.getImage());
+        }
 	}
 	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == SELECT_PICTURE && resultCode == RESULT_OK)
+		{
+			if(data.getData() != null) // Gallery returned data
+			{
+				Uri selectedImage = data.getData();
+				
+				
+				ParcelFileDescriptor parcelFD;
+				try
+				{
+					parcelFD = getContentResolver().openFileDescriptor(selectedImage, "r");
+					FileDescriptor fd = parcelFD.getFileDescriptor();
+					image = BitmapFactory.decodeFileDescriptor(fd);
+				} catch (FileNotFoundException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			else // Camera returned data
+			{
+				Bundle extras = data.getExtras();
+				image = (Bitmap) extras.get("data");
+			}
+			
+			imageView.setImageBitmap(image);
+		}
+	    
+	}
 	
 	
 }
--- MedMemory/src/com/example/medmemory/Meds.java
+++ MedMemory/src/com/example/medmemory/Meds.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.medmemory.adapter.MedicationAdapter;
@@ -14,8 +16,10 @@ import com.example.medmemory.model.Medication;
 
 public class Meds extends Activity {
 
+	private static final int ADD_MED_REQUEST = 69; // the sex number
+	
 	ListView listView;
-	ArrayList<Medication> medications; //yeah its not plural f the police
+	ArrayList<Medication> medications; // yeah its not plural f the police
 	MedicationAdapter adapter;
 	
 	@Override
@@ -23,18 +27,46 @@ public class Meds extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meds);
         
+        // Retrieve all meds
         Database.context = this;
         medications = Database.getAllMedications();
         
+        // Set up ListView
         listView = (ListView) findViewById(R.id.med_list);
         adapter = new MedicationAdapter(this, R.layout.med_list_row, medications);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+        	@Override
+        	public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+        			long arg3)
+        	{
+        		Intent editIntent = new Intent(Meds.this, AddMedication.class);
+        		editIntent.putExtra(AddMedication.MEDICATION_ID, medications.get(position).getId());
+        		startActivityForResult(editIntent, ADD_MED_REQUEST);
+        	}
+		});
         
 	}
 	
 	public void gotoaddmedication(View view){
     	Intent myintent = new Intent(this, AddMedication.class);
-    	startActivity(myintent);
+    	startActivityForResult(myintent, ADD_MED_REQUEST);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == ADD_MED_REQUEST && resultCode == RESULT_OK)
+		{
+	        // Re-retrieve all meds
+	        Database.context = this;
+	        medications = Database.getAllMedications();
+	        
+	        // Replace adapter it's not efficient but who cares
+	        adapter = new MedicationAdapter(this, R.layout.med_list_row, medications);
+	        listView.setAdapter(adapter);
+		}
 	}
 	
 }
